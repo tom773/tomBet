1a5326398818f12ef5e283a30b63d2e55a80d18d
tom773 <tom.matthews68@yahoo.com.au>
2024-03-12 22:47:45 +1100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+#![allow(unused)]
+
+use axum::extract::{Path, Query};
+use axum::response::Html;
+use axum::Router;
+use axum::routing::{get, get_service};
+use tokio::net::TcpListener;
+use std::net::SocketAddr;
+use axum::response::IntoResponse;
+use serde::Deserialize;
+use tower_http::services::ServeDir;
+use rand::Rng;
+use askama::Template;
+use std::slice::Iter;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().merge(routes_hello());
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+	println!("->> LISTENING on {:?}\n", listener.local_addr());
+	axum::serve(listener, routes_hello.into_make_service())
+		.await
+		.unwrap();
+}
+
+// Practice Routes
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/lotto", get(handler_lotto))
+}
+
+async fn handler_lotto() -> impl IntoResponse {
+    let mut nums = vec![];
+    for x in 1..8 {
+        nums.push(rand::thread_rng().gen_range(1..44));
+    }
+    let admin = User {
+        name: String::from("Admin"),
+        age: 69,
+    };
+    let template = MyTemplate {
+        name: &admin.name,
+        age: &admin.age,
+        nums: &nums,
+    };
+    Html(template.render().unwrap())
+}
+
+#[derive(Template)]
+#[template(path = "index.html", escape = "none")]
+pub struct MyTemplate<'a> {
+    name: &'a String,
+    age: &'a u8,
+    nums: &'a Vec<i32>,
+}
+
+// Types
+
+struct User {
+    name: String,
+    age: u8,
+}
