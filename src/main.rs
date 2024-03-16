891ab565d41fd78d59d19647c61384e77f44dd8d
tom773 <tom.matthews68@yahoo.com.au>
2024-03-16 18:11:42 +1100

    Added Landing Page

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 use axum::extract::{Path, Query};
 use axum::response::Html;
 use axum::Router;
-use axum::routing::{get, get_service};
+use axum::http::{StatusCode};
+use axum::routing::{get, get_service, post};
 use tokio::net::TcpListener;
 use std::net::SocketAddr;
 use axum::response::IntoResponse;
@@ -15,7 +16,8 @@ use std::slice::Iter;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().merge(routes_hello());
+    let routes_hello = Router::new().merge(routes_hello())
+        .nest_service("/public", ServeDir::new("public"));
 
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
 	println!("->> LISTENING on {:?}\n", listener.local_addr());
@@ -27,7 +29,15 @@ async fn main() {
 // Practice Routes
 fn routes_hello() -> Router {
     Router::new()
+        .route("/", get(handler_index))
         .route("/lotto", get(handler_lotto))
+}
+
+async fn handler_index() -> impl IntoResponse{
+    let tmpl = HomeTmpl{
+        dummyData: &"Dummy".to_string(),
+    };
+    return Html(tmpl.render().unwrap());    
 }
 
 async fn handler_lotto() -> impl IntoResponse {
@@ -55,11 +65,17 @@ async fn handler_lotto() -> impl IntoResponse {
 }
 
 #[derive(Template)]
-#[template(path = "index.html", escape = "none")]
+#[template(path = "lotto.html", escape = "none")]
 pub struct MyTemplate<'a> {
     name: &'a String,
     age: &'a u8,
     nums: &'a Vec<i32>,
+}
+
+#[derive(Template)]
+#[template(path = "index.html", escape = "none")]
+pub struct HomeTmpl<'a>{
+    dummyData: &'a String,
 }
 
 // Types
